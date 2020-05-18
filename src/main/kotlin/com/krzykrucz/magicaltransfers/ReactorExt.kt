1a42efdc278ad262ce982f8366782b16cb9c8ed5
krzykrucz <krzysiuk.66@gmail.com>
2020-05-18 11:49:23 +0200

    cleanup

--- src/main/kotlin/com/krzykrucz/magicaltransfers/ReactorExt.kt
+++
@@ -1,7 +0,0 @@
-package com.krzykrucz.magicaltransfers
-
-import reactor.core.publisher.Mono
-import reactor.kotlin.core.publisher.switchIfEmpty
-import reactor.kotlin.core.publisher.toMono
-
-fun <T> Mono<T>.failIfEmpty(exception: Throwable): Mono<T> = switchIfEmpty { exception.toMono() }
