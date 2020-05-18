454f53aa4f82b96bb9e8f7f4be14a66f901f1ccc
krzykrucz <krzysiuk.66@gmail.com>
2020-05-18 12:19:14 +0200

    mvn repo

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,6 +17,7 @@ java.sourceCompatibility = JavaVersion.VERSION_11
 repositories {
     mavenCentral()
     maven { url = uri("https://repo.spring.io/milestone") }
+    maven { url = uri("https://dl.bintray.com/konrad-kaminski/maven") }
 }
 
 dependencies {
