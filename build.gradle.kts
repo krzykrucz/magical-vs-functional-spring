ab5cb7b7ea77198f8ec77bcb715aba8d06af64b6
Krzysiek Kruczyński <krzysiuk.66@gmail.com>
2020-05-18 11:47:19 +0200

    spring coroutines deps

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,12 +24,13 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-mongodb-reactive")
     implementation("org.springframework.boot:spring-boot-starter-security")
     implementation("org.springframework.boot:spring-boot-starter-webflux")
+    implementation("org.springframework.kotlin:spring-boot-autoconfigure-kotlin-coroutine:0.3.7")
+    implementation("org.springframework.kotlin:spring-data-mongodb-kotlin-coroutine:0.3.7")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("io.projectreactor.kotlin:reactor-kotlin-extensions")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
-    implementation("org.springframework.fu:spring-fu-kofu:0.3.0.M5")
     testImplementation("org.springframework.boot:spring-boot-starter-test") {
         exclude(group = "org.junit.vintage", module = "junit-vintage-engine")
     }
